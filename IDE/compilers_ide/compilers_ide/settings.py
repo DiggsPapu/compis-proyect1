056d7da0a4eb16d76cca4768b02424fd29034422
FernandezDL <dianaluciafv@gmail.com>
2024-08-25 22:15:44 -0600

    Update: IDE
    Agregando el front-end para el IDE

--- IDE/compilers_ide/compilers_ide/settings.py
+++ IDE/compilers_ide/compilers_ide/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "compiler/static",  # Ajusta este path a donde se encuentren tus archivos estáticos
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
