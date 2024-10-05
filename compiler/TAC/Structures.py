5742d5428c262794635fcf35eba7127cc6e5bbc4
DiggsPapu <diggs.papu@gmail.com>
2024-10-04 18:56:36 -0600

    feat: Initialize in the constructor the atributes of the Cuadrupleta class

--- compiler/TAC/Structures.py
+++ compiler/TAC/Structures.py
@@ -1,7 +1,7 @@
 class Cuadrupleta():
-    def __init__(self) -> None:
-        self.arg1 = None
-        self.arg2 = None
-        self.operacion = None
-        self.resultado = None
+    def __init__(self, arg1=None, arg2=None, operacion=None, resultado=None) -> None:
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.operacion = operacion
+        self.resultado = resultado
         
