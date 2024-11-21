63c5334f3555812e2ace14d420a5fa92e31f0a70
FernandezDL <dianaluciafv@gmail.com>
2024-11-20 19:25:19 -0600

    Revert "Add: mip funciones parcial"
    This reverts commit cc8057f69a98f4d6928415c2e8a0ad4da0a6e928.

--- compiler/MIPS/Mips_output.asm
+++ compiler/MIPS/Mips_output.asm
@@ -1,29 +1,43 @@
 .data
+str_Global_Variable_inside_function: .asciiz "Global Variable inside function"
+str_$t0: .space 256
 .text
-countToTen:
-li $t2, 0  # Assign 0 to i
-li $t2, 0  # Assign 0 to i
-L0:
-li $t4, 10  # Cargar literal 10
-slt $t0, $t2, $t4  # $t0 = (i < 10)
-bne $t0, $zero, L1  # Conditional jump
-j L2
-L1:
-move $a0, $t2  # Mover valor de i a $a0
+main:
+li $t1, 20  # Assign 20 to globalVar1
+la $t2, str_Global_Variable_inside_function  # Cargar direccion de cadena Global Variable inside function
+move $t0, $t2  # Cargar $t2
+move $t1, $t1  # Cargar $t1
+la $t2, str_$t0  # Cargar dirección para resultado
+
+
+# Concatenación de cadenas
+move $a0, $t0  # Primera cadena
+li $v0, 4      # syscall para imprimir cadena
+syscall
+
+li $a0, 32     # Cargar el valor ASCII del espacio en blanco en $a0
+li $v0, 11     # syscall para imprimir un carácter
+syscall        # Llamada al sistema
+
+move $a0, $t1  # Segunda cadena
+li $v0, 1      # syscall para imprimir cadena
+syscall
+move $t0, $t2  # Guardar dirección del resultado en $t0
+li $a0, 10
+li $v0, 11
+syscall
+move $a0, $t0  # Mover valor de $t0 a $a0
+li $v0, 4  # syscall para imprimir concatenación
+syscall
+li $a0, 10
+li $v0, 11
+syscall
+move $a0, $t1  # Mover valor de globalVar1 a $a0
 li $v0, 1  # syscall para imprimir entero
 syscall
-li $t4, 1  # Cargar literal 1
-add $t1, $t2, $t4  # $t1 = i + 1
-move $t2, $t1  # Assign $t1 to i
-move $t2, $t2  # Assign i to i
-j L0
-L2:
-# End of function
-lw $ra, 4($sp)  # Restore return address
-lw $fp, 0($sp)  # Restore frame pointer
-addiu $sp, $sp, 8  # Adjust stack pointer
-jr $ra  # Return to caller
-main:
-jal countToTen  # Saltar a la direccion de la funcion
-# Liberar parametros de la pila
-addi $sp, $sp, 0  # Ajustar la pila para 0 parámetros
+li $a0, 10
+li $v0, 11
+syscall
+li $a0, 5  # Cargar valor inmediato 5
+li $v0, 1  # syscall para imprimir entero
+syscall
